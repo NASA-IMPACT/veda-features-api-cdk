bd31db699e150839a0cb12b44b80814a0eb9c256
Alexandra Kirk <alexandra@developmentseed.org>
2024-11-18 16:52:45 -0700

    mangum lifespan on

--- features_api/runtime/handler.py
+++ features_api/runtime/handler.py
@@ -11,7 +11,7 @@ from src.monitoring import logger, metrics, tracer
 logging.getLogger("mangum.lifespan").setLevel(logging.DEBUG)
 logging.getLogger("mangum.http").setLevel(logging.DEBUG)
 
-handler = Mangum(app, lifespan="auto")
+handler = Mangum(app, lifespan="on")
 
 if "AWS_EXECUTION_ENV" in os.environ:
     loop = asyncio.get_event_loop()
