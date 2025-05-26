e1d8a20fe77a97895b4ce36eb731e92f1c6849c4
smohiudd <saadiq@developmentseed.org>
2025-05-26 11:51:53 -0600

    update sql

--- features_api/runtime/sql/functions.sql
+++ features_api/runtime/sql/functions.sql
@@ -1,3 +1,5 @@
-CREATE OR REPLACE VIEW pg_temp.eis_fire_snapshot_perimeter_nrt_latest AS
-SELECT *
-FROM public.eis_fire_snapshot_perimeter_nrt;
+CREATE OR REPLACE VIEW eis_fire_lf_perimeter_nrt_latest AS
+SELECT DISTINCT ON (fireid) 
+    fireid, t, region, duration, farea, meanfrp, fperim, n_pixels, n_newpixels, pixden, primarykey, geometry
+FROM public.eis_fire_lf_perimeter_nrt
+ORDER BY fireid, t DESC;
