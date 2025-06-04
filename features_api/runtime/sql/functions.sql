5b912fef803c77191f1c5704a74afc3ca4d88459
smohiudd <saadiq@developmentseed.org>
2025-06-04 10:54:47 -0600

    make sql view conditional

--- features_api/runtime/sql/functions.sql
+++ features_api/runtime/sql/functions.sql
@@ -1,5 +1,10 @@
-CREATE OR REPLACE VIEW eis_fire_lf_perimeter_nrt_latest AS
-SELECT DISTINCT ON (fireid) 
-    fireid, t, region, duration, farea, meanfrp, fperim, n_pixels, n_newpixels, pixden, primarykey, geometry
-FROM public.eis_fire_lf_perimeter_nrt
-ORDER BY fireid, t DESC;
+DO $$
+BEGIN
+  IF EXISTS (SELECT FROM information_schema.tables WHERE table_schema = 'public' AND table_name = 'eis_fire_lf_perimeter_nrt') THEN
+    CREATE OR REPLACE VIEW eis_fire_lf_perimeter_nrt_latest AS
+    SELECT DISTINCT ON (fireid) 
+        fireid, t, region, duration, farea, meanfrp, fperim, n_pixels, n_newpixels, pixden, primarykey, geometry
+    FROM public.eis_fire_lf_perimeter_nrt
+    ORDER BY fireid, t DESC;
+  END IF;
+END $$;
