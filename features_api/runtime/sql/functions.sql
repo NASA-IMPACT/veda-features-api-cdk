3037883f6563fec6b662f0a02fec491be062f1d3
smohiudd <saadiq@developmentseed.org>
2025-09-25 11:04:34 -0600

    checks of temp view exists

--- features_api/runtime/sql/functions.sql
+++ features_api/runtime/sql/functions.sql
@@ -1,7 +1,19 @@
 DO $$
 BEGIN
-  IF EXISTS (SELECT FROM information_schema.tables WHERE table_schema = 'public' AND table_name = 'eis_fire_lf_perimeter_nrt') THEN
-    CREATE OR REPLACE VIEW eis_fire_lf_perimeter_nrt_latest AS
+  IF EXISTS (
+    SELECT
+    FROM information_schema.tables
+    WHERE table_schema = 'public'
+      AND table_name   = 'eis_fire_lf_perimeter_nrt'
+  )
+  AND NOT EXISTS (
+    SELECT
+    FROM information_schema.views
+    WHERE table_schema = 'pg_temp'
+      AND table_name   = 'eis_fire_lf_perimeter_nrt_latest'
+  )
+  THEN
+    CREATE VIEW eis_fire_lf_perimeter_nrt_latest AS
     SELECT DISTINCT ON (fireid) 
         fireid, t, region, duration, farea, meanfrp, fperim, n_pixels, n_newpixels, pixden, primarykey, geometry
     FROM public.eis_fire_lf_perimeter_nrt
