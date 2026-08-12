00adcc4771265015a389163030aab15157a5c193
aliziel <21992503+aliziel@users.noreply.github.com>
2026-08-12 13:59:30 -0700

    fix: allow rollback on failed create

--- features_api_database/infrastructure/construct.py
+++ features_api_database/infrastructure/construct.py
@@ -181,7 +181,7 @@ class FeaturesRdsConstruct(Construct):
             "instance_type": rds_instance_type,
             "vpc_subnets": self.vpc_subnets,
             "deletion_protection": features_db_settings.rds_deletion_protection,
-            "removal_policy": RemovalPolicy.RETAIN,
+            "removal_policy": RemovalPolicy.RETAIN_ON_UPDATE_OR_DELETE,
             "publicly_accessible": features_db_settings.publicly_accessible,
             "parameter_group": parameter_group,
         }
